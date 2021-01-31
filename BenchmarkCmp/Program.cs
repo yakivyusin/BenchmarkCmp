cc871e9ae3095cdd2d2969175fb9b808799a266f
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-01-31 15:09:44 +0200

    Added Program Flow with Two Comparers

--- BenchmarkCmp/Program.cs
+++ BenchmarkCmp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BenchmarkCmp.Comparers;
+using BenchmarkCmp.DataLoaders;
+using System;
+using System.Reflection;
+using YetAnotherConsoleTables;
 
 namespace BenchmarkCmp
 {
@@ -6,7 +10,45 @@ namespace BenchmarkCmp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 2)
+            {
+                var versionString = Assembly
+                    .GetEntryAssembly()
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                    .InformationalVersion
+                    .ToString();
+
+                Console.WriteLine($"benchmarkcmp v{versionString}");
+                Console.WriteLine("Usage:");
+                Console.WriteLine("    benchmarkcmp <file1> <file2>");
+
+                return;
+            }
+
+            var data1 = DataLoaderFactory.GetLoader(args[0]).LoadBenchmarks(args[0]);
+            var data2 = DataLoaderFactory.GetLoader(args[1]).LoadBenchmarks(args[1]);
+
+            var meanComparedData = new MeanComparer().Compare(data1, data2);
+            ConsoleTable.From(meanComparedData).Write(new TableFormatting());
+
+            var allocatedBytesComparedData = new AllocatedComparer().Compare(data1, data2);
+
+            if (allocatedBytesComparedData.Length > 0)
+            {
+                Console.WriteLine();
+                ConsoleTable.From(allocatedBytesComparedData).Write(new TableFormatting());
+            }
+        }
+
+        private class TableFormatting : ConsoleTableFormat
+        {
+            public TableFormatting() : base(
+                columnDelimiter: '|',
+                intersection: '+',
+                borders: Borders.HeaderDelimiter)
+            {
+
+            }
         }
     }
 }
