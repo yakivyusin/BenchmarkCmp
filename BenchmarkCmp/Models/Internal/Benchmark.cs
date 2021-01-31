0d47696475553d16fbc16d2d69ce200bb76f8344
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-01-31 13:56:11 +0200

    Added Internal Model Classes

---
+++ BenchmarkCmp/Models/Internal/Benchmark.cs
@@ -0,0 +1,9 @@
+﻿namespace BenchmarkCmp.Models.Internal
+{
+    public class Benchmark
+    {
+        public string Name { get; set; }
+        public double Mean { get; set; }
+        public int? AllocatedBytes { get; set; }
+    }
+}
