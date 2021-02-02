d0c84aa47966059e3e65beab1c7abc7b33e8d6c2
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-02-02 17:36:59 +0200

    Added Allocated Bytes Converter

---
+++ BenchmarkCmp/Converters/BytesOutputConverter.cs
@@ -0,0 +1,20 @@
+﻿using ByteSizeLib;
+using YetAnotherConsoleTables;
+
+namespace BenchmarkCmp.Converters
+{
+    public class BytesOutputConverter : TableMemberConverter<double>
+    {
+        public override string Convert(double value)
+        {
+            if (value == 0)
+            {
+                return "0";
+            }
+
+            var byteSize = ByteSize.FromBytes(value);
+
+            return $"{byteSize.LargestWholeNumberBinaryValue:0.0000} {byteSize.LargestWholeNumberBinarySymbol}";
+        }
+    }
+}
--- BenchmarkCmp/Models/Output/BenchmarkAllocatedComparing.cs
+++ BenchmarkCmp/Models/Output/BenchmarkAllocatedComparing.cs
@@ -1,13 +1,16 @@
-﻿using YetAnotherConsoleTables.Attributes;
+﻿using BenchmarkCmp.Converters;
+using YetAnotherConsoleTables.Attributes;
 
 namespace BenchmarkCmp.Models.Output
 {
     public class BenchmarkAllocatedComparing : BenchmarkComparingBase
     {
         [TableMember(DisplayName = "old allocs", Order = 2)]
+        [TableMemberConverter(typeof(BytesOutputConverter))]
         public override double ValueOld { get; set; }
 
         [TableMember(DisplayName = "new allocs", Order = 3)]
+        [TableMemberConverter(typeof(BytesOutputConverter))]
         public override double ValueNew { get; set; }
     }
 }
