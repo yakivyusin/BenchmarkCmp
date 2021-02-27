71c99b6d35c1f196239e445039c50c635cbd9b4d
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-02-27 17:46:40 +0200

    Added Json Full Input Model for Extra Metrics

---
+++ BenchmarkCmp/Models/Input/Json/BenchmarkMetric.cs
@@ -0,0 +1,8 @@
+﻿namespace BenchmarkCmp.Models.Input.Json
+{
+    public class BenchmarkMetric
+    {
+        public double Value { get; set; }
+        public BenchmarkMetricDescriptor Descriptor { get; set; }
+    }
+}
